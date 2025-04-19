2981988cff4c996b7a51e71fb82c6b97a94470b9
divyansh-1503 <divyansh.bhola15@gmail.com>
2025-04-19 14:14:44 +0530

    web

--- main.js
+++ main.js
@@ -5,3 +5,10 @@
 //     backDelay: 1000,
 //     loop:true
 // })
+const mobileMenu = document.querySelector('.mobile-menu');
+const navbar = document.querySelector('.navbar');
+
+mobileMenu.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    mobileMenu.classList.toggle('open'); // Optional: for changing the icon
+});
